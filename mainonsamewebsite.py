8b5df23b6861c34cde1099c3c833bc46081f7ffa
Jeevananthan S <81102317+Jeevananthan1712@users.noreply.github.com>
2022-12-10 23:42:01 +0530

    ₹ separated

--- mainonsamewebsite.py
+++ mainonsamewebsite.py
@@ -10,18 +10,29 @@ with open('myact.html', 'r') as html_file:
     # t = tag.find('h2')
     # print(t)
     countpaid = 0
+    addingpaid = 0
     countrev = 0
     for tag in tags:
-        sentence = tag.text
-        num = tag.text.split()[1].split(" ")[-1]
-        conditions =  tag.text.split()[0]
-        print(sentence)
-        print(num)
-        print(conditions)
+        sentence = tag.text.replace('Sent,', 'a')
+        mainpart  = sentence.split("using")[0]
+        num = tag.text.split(" ")[1].replace('â','₹')
+        numm = num.split(".")[0]
+        conditions = tag.text.split()[0]
+        # print(numm)
+
+        # print(sentence)
+        print(mainpart)
+        # print(num)
+        # print(conditions)
         # print(f'{ta} last_number {t}')
         if(conditions == 'Paid' or  conditions == 'Sent'):
             countpaid = countpaid + 1
+            # addingpaid = int(addingpaid + int( numm))
+            print(numm)
+            # print(addingpaid)
         else:
             countrev = countrev + 1
-        print(countpaid)
-        print(f'{countrev} r')
+            print(f'{numm} received')
+
+        # print(countpaid)
+    # print(f'sent or paid someone {countpaid} times and received money from someone {countrev} times')
